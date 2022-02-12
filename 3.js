a66510b58e44d47de41ff0e1c462e3c1598527cd
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-02-12 11:52:09 +0300

    вывод типов данных

---
+++ 3.js
@@ -0,0 +1,17 @@
+let chislo=500;
+console.log(typeof chislo);
+let name='Nadya';
+console.log(typeof name);
+
+let proverka=true;
+console.log(typeof proverka);
+
+let abc="Privet11";
+console.log(typeof abc);
+
+let name1 = { }; 
+console.log( typeof name1); 
+
+let age; 
+console.log(typeof age); 
+
